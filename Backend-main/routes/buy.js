5c5e5fa6c0ad78eb18481897e7161cdb53a11745
AIChE-RGIPT <aiche@rgipt.ac.in>
2024-08-15 15:18:48 +0530

    Update buy.js

--- Backend-main/routes/buy.js
+++ Backend-main/routes/buy.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const buyContact = require("../controller/buy.js");
 
 router.post("/buy", buyContact);
+router.get("/buy", (req,res)=>{res.send("Buy router established")});
 
 module.exports = router;
