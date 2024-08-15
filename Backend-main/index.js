45487d99664d05c96204cac3ac474c48a056ffa4
AIChE-RGIPT <aiche@rgipt.ac.in>
2024-08-15 13:53:51 +0530

    Update index.js

--- Backend-main/index.js
+++ Backend-main/index.js
@@ -11,7 +11,7 @@ let port = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin : ["https://www.aichergipt.com/Contact" , "https://www.aichergipt.com/Registration"],
+        origin : ["https://www.aichergipt.com" , "https://www.aichergipt.com/Contact" , "https://www.aichergipt.com/Registration" ],
         methods : ["POST", "GET"],
         credentials :true
     }
