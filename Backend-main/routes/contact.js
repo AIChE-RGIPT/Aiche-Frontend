f470cb2817774b69a4439f01a36b1807efb09fe0
AIChE-RGIPT <aiche@rgipt.ac.in>
2024-08-15 15:19:04 +0530

    Update contact.js

--- Backend-main/routes/contact.js
+++ Backend-main/routes/contact.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const saveContact = require("../controller/contact.js");
 
 router.post("/contact", saveContact);
+router.get("/contact", (req, res) => { res.send("contact router established") });
 
 module.exports = router;
