43bbf71ec1eb0024b7dc60029c1884a347420f09
AIChE-RGIPT <aiche@rgipt.ac.in>
2024-08-15 14:26:55 +0530

    Update index.js

--- Backend-main/index.js
+++ Backend-main/index.js
@@ -11,7 +11,7 @@ let port = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin : ["https://www.aichergipt.com" , "https://www.aichergipt.com/Contact" , "https://www.aichergipt.com/Registration" ,"https://aiche-rgipt-api.vercel.app/Registration" , "https://aiche-rgipt-api.vercel.app/Contact" ,"https://aiche-rgipt-api.vercel.app" ],
+        origin : ["https://aiche-rgipt-api.vercel.app/Registration" , "https://aiche-rgipt-api.vercel.app/Contact" ,"https://aiche-rgipt-api.vercel.app" ],
         methods : ["POST", "GET"],
         credentials :true
     }
